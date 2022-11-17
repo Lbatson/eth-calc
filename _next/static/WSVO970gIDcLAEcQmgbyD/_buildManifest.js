8f926ccf80827ba6300f4831122e946c60548f12
Lbatson <Lbatson@users.noreply.github.com>
2022-11-17 04:12:01 +0000

    Deploying to gh-pages from @ Lbatson/eth-calc@2fff7d87fabaa848dd0923f326705f646b2383b4 🚀

---
+++ _next/static/WSVO970gIDcLAEcQmgbyD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/612-069a3bdedc861bec.js","static/css/ca0a4dc70ba482a8.css","static/chunks/pages/index-ba0c8efc186b0e07.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
